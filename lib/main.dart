24dc077a8f8baae503845196c4773c3ad358f5bf
dheerajraina <dheerajraina55@gmail.com>
2021-12-18 17:22:05 +0530

    first day working on UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,224 @@
-import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
+
+import 'package:flutter/material.dart';
+// import 'package:html/dom.dart';
+
+void main(){
+  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
+class MyApp extends StatelessWidget {
+  // const MyApp({ Key? key }) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      title: "Netflix",
+debugShowCheckedModeBanner: false,
+    // darkTheme: ThemeData(),
+     theme:ThemeData(
+       primaryColorDark: Colors.black,
+       brightness:Brightness.dark,
+     ),
+      home: HomePage(),     
+     
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class HomePage extends StatefulWidget {
+  const HomePage({ Key? key }) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _HomePageState createState() => _HomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      backgroundColor: Theme.of(context).primaryColorDark,
+     body:SingleChildScrollView(/* converts whole page into a scroll view */
+       scrollDirection: Axis.vertical,
+       
+       child: Column(
+         children: [
+           Container(
+            // color: Colors.pink,
+            //  width: MediaQuery.of(context).size.width,
+             height: MediaQuery.of(context).size.height*0.7,
+            //  margin: EdgeInsets.only(left: 3),
+              decoration: BoxDecoration(
+                // borderRadius: BorderRadius.circular(20),
+                // color: Colors.red,
+              ),
+            // margin:EdgeInsets.only(top: 10),
+             child: Card(
+               
+               child: Stack(/* Stack to place items on over another*/
+                 children: [
+                   
+                   ClipRRect(
+                     child: Image.network(
+                       'https://m.media-amazon.com/images/M/MV5BN2ZmYjg1YmItNWQ4OC00YWM0LWE0ZDktYThjOTZiZjhhN2Q2XkEyXkFqcGdeQXVyNjgxNTQ3Mjk@._V1_.jpg',
+     
+                       height: MediaQuery.of(context).size.height,
+                       width: MediaQuery.of(context).size.width,
+                       fit: BoxFit.fill,
+                     ),
+                   ),
+                  //  SizedBox(
+                  //    height:MediaQuery.of(context).size.height*1,
+                  //    width:MediaQuery.of(context).size.width*1,
+                  //  ),
+                   Row(
+                     children: [
+                      Padding(padding: EdgeInsets.only(top:MediaQuery.of(context).size.height*0.29,left: MediaQuery.of(context).size.width*0.08  )),
+                      Image.network(
+                        "https://img.icons8.com/external-tal-revivo-color-tal-revivo/96/000000/external-netflix-an-american-video-on-demand-service-logo-color-tal-revivo.png",
+                        height: MediaQuery.of(context).size.width*0.1,
+                      ),
+     
+                      SizedBox(
+                    //  height:MediaQuery.of(context).size.height*0.2,
+                     width:MediaQuery.of(context).size.width*0.09,
+                   ),
+                       Text(
+                     "TV Shows",
+                     style: TextStyle(
+                       letterSpacing: MediaQuery.of(context).size.width*0.002,
+                       color: Colors.white,
+                       fontWeight:FontWeight.bold,
+                       fontSize: MediaQuery.of(context).size.width*0.043,
+                     ),
+                   ),
+                   SizedBox(
+                    //  height:MediaQuery.of(context).size.height*0.2,
+                     width:MediaQuery.of(context).size.width*0.07,
+                   ),
+     
+                   Text(
+                     "Movies",
+                     style: TextStyle(
+                       letterSpacing: MediaQuery.of(context).size.width*0.002,
+                       color: Colors.white,
+                       fontWeight:FontWeight.bold,
+                       fontSize: MediaQuery.of(context).size.width*0.043,
+                     ),
+                   ),
+                   SizedBox(
+                    //  height:MediaQuery.of(context).size.height*0.2,
+                     width:MediaQuery.of(context).size.width*0.07,
+                   ),
+     
+                   Text(
+                     "My List",
+                     style: TextStyle(
+                       letterSpacing: MediaQuery.of(context).size.width*0.002,
+                       color: Colors.white,
+                       fontWeight:FontWeight.bold,
+                       fontSize: MediaQuery.of(context).size.width*0.043,
+                     ),
+                   )
+
+                  
+                     ],
+                   ),
+                    
+                 ],
+               ),
+             ),
+           ),
+           Row(
+                      children: [
+                        
+                        SizedBox(
+                          //  height:MediaQuery.of(context).size.height*0.01,
+                     width:MediaQuery.of(context).size.width*0.15,
+                        ),
+                        IconButton(
+                         onPressed:(){}, 
+                         icon:Icon(Icons.add)),
+                      SizedBox(
+                          //  height:MediaQuery.of(context).size.height*0.01,
+                     width:MediaQuery.of(context).size.width*0.1,
+                        ),
+                      Container(
+                        // color: Colors.white,
+                        width:MediaQuery.of(context).size.width*0.26 ,
+                        height: MediaQuery.of(context).size.height*0.04,
+                        decoration:BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(9)
+
+                        ),
+                        child: Row(
+                          children: [
+                             SizedBox(
+                          //  height:MediaQuery.of(context).size.height*0.01,
+                     width:MediaQuery.of(context).size.width*0.01,
+                        ),
+                            IconButton(
+                              padding: EdgeInsets.only(bottom: 3),
+                              onPressed: (){}, 
+                              icon: Icon(
+                                Icons.play_arrow_rounded,
+                                color: Colors.black,
+                                size:MediaQuery.of(context).size.height*0.03 ,
+                                
+                                ),),
+
+                                Text(
+                                  'Play',
+                                  style: TextStyle(
+                                    color: Colors.black,
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: MediaQuery.of(context).size.height*0.02,
+                                    letterSpacing: MediaQuery.of(context).size.height*0.001,
+                                  ),
+                                  ),
+                          ],
+                        ),
+
+                      ),
+                      SizedBox(
+                          //  height:MediaQuery.of(context).size.height*0.01,
+                     width:MediaQuery.of(context).size.width*0.1,
+                        ),
+                        IconButton(
+                          onPressed: (){}, 
+                          icon: Icon(
+                            Icons.info_outline
+                            ),
+                            )
+                      ],
+                    ),
+
+
+            SizedBox(
+                     height:MediaQuery.of(context).size.height*0.03,
+                     width:MediaQuery.of(context).size.width*0.07,
+                   ),
+         Row(
+           children: [
+             Padding(padding:EdgeInsets.only(left:MediaQuery.of(context).size.width*0.03, )),
+             Text(
+               "Popular on Netflix",
+               textAlign: TextAlign.left,
+               style: TextStyle(
+                 fontSize: MediaQuery.of(context).size.width*0.04,
+                 fontWeight: FontWeight.bold,
+                 letterSpacing: MediaQuery.of(context).size.width*0.002,
+               ),
+               ),
+           ],
+         ),
+         ],
+       ),
+     ),
+      
     );
   }
 }
